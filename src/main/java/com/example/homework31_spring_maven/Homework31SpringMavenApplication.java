28076acf0b42545878c09b817ebb13365e534d79
LUM <lumlum177@gmail.com>
2022-11-14 17:50:51 +0300

    Начальный коммит

---
+++ src/main/java/com/example/homework31_spring_maven/Homework31SpringMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework31_spring_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework31SpringMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework31SpringMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework31_spring_maven/Homework31SpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework31_spring_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework31SpringMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
